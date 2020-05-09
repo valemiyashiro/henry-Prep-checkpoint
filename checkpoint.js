39c579d7d78dbb99f2ffb37aa11734ed6c5b3834
Valeria <valeriamiyashiro.vm@gmail.com>
2020-05-09 14:27:25 -0300

    checkpoint

--- checkpoint.js
+++ checkpoint.js
@@ -5,6 +5,8 @@ function menorMayor(numeros) {
   // Escribi una función llamada menorMayor que tome como entrada un arreglo de números y devuelva un arreglo 
   // que contenga el menor número del arreglo en la posición cero y el mayor número del arreglo en la posición 1.
   // Ej: menorMayor([4, 6, 1, 7, 15]) // retorna [1, 15]
+  var max=Math.max.apply(null,numeros);
+  var min=Math.min.apply(null,numeros);
 
 }
 
@@ -14,7 +16,13 @@ function stringMasLarga(strings) {
   // Ej:
   // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
-
+  var largo= strings[0];
+  for(var i=0; i<strings.length;i++){
+    if(strings[i]>largo){
+      largo=strings[i];
+    }
+  }
+  return largo;
 }
 
 function buscarAmigo(amigos, nombre) {
@@ -24,7 +32,7 @@ function buscarAmigo(amigos, nombre) {
   // Ej:
   //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
   //  buscarAmigo(amigos, 'toni') // retorna { nombre: 'toni', edad: 33 };
-  
+
 }
 
 function sumArray(array, n) {
@@ -35,7 +43,15 @@ function sumArray(array, n) {
   // sumArray([2,5,7,10,11,15,20], 13)  // retorna true     2+11 suman 13
   // sumArray([2,5,7,10,11,15,20], 14)  // retorna false
   // pista: podes usar bucles anidados;
-  
+  var suma=0;
+  for(var i=0;i<array.length;i++){
+    for(var j=i+1;j<array.length;j++){
+      if(array[i]+ array[j]=== n){
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 function pluck(array, propiedad) { 
@@ -46,8 +62,6 @@ function pluck(array, propiedad) {
   // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
   // productos.pluck(productos, 'name') // ['TV LCD', 'Computadora']
   // pista: es una buena oportunidad para usar map.
-  
-  
 }
 
 // =======================================================================
@@ -61,34 +75,41 @@ function crearClasePersona() {
 
   class Persona {
     constructor(nombre, edad, hobbies, amigos) {
-      
+      this.nombre= nombre;
+      this.edad= edad;
+      this.hobbies= hobbies;
+      this.amigos= amigos;
     }
 
     addFriend(nombre, edad) {
       // el metodo addFriend recibe un string nombre y un entero edad y debe agregar un objeto:
       // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
       // no debe retornar nada.
-      
+      this.amigos.push(edad,nombre);
     }
 
     addHobby(hobby) {
       // este método debe agregar un hobby (hobby) al arreglo de hobbies de la persona.
       // no debe retornar nada.
-      
+      this.hobbies.push(hobby);
     }
     getFriends() {
       // Escribe una función que retorne un arreglo con sólo los nombres del arreglo de amigos
       // de la persona.
       // Ej:
       // persona.getFriends() // retorna ['toni', 'Leo', 'Manu']
-      
+      var nuevoArr=[0];
+      for(var i=1;i< this.amigos.length;i++){
+          nuevoArr= this.amigos[i].nombre;
+      }
+      return nuevoArr;
     }
 
     getHobbies() {
       // Escribe una función que retorne un arreglo con los hobbies de la persona
       // Ej:
       // persona.getHobbies() // retorna ['correr', 'dormir', 'nadar']
-      
+      return this.hobbies;
     }
 
     getPromedioEdad() {
@@ -104,6 +125,13 @@ function crearClasePersona() {
       //   }]
       // }
       // persona.getPromedioEdad() // retorna 29
+      var suma=0;
+      var promedio=0;
+      for(var i=0;i<this.amigos.length;i++){
+        suma=suma+this.amigos[i].edad;
+      }
+      promedio=suma/this.amigos.length;
+      return promedio;
     }
   };
 
--- teoria.md
+++ teoria.md
@@ -1,24 +1,24 @@
 ## Explica los siguentes conceptos con tus propias palabras. (no más de tres líneas por respuesta).
 
-* HTML:
+* HTML: es un lenguaje formado por elementos,el cual se utiliza para poder darle una estructura al sitio web,estos elementos estan formados por contenidos,atributos y etiquetas.
 
-* CSS:
+* CSS:es un lenguaje utilizado en la presentacion de documentos como HTML o XHTML,permite modificar la apariencia de una pagina web utilizando elementos como fondo,color,tamaño,bordes,margen,etc.
 
-* Git:
+* Git:es un software de control de versiones, es decir cuando estas desarrollando  una pagina web o un software.Esto permite subir el codigo a la nube y poder trabajar con un grupo de personas.
 
-* Arreglos:
+* Arreglos: son un conjunto de datos ordenados por posiciones, que se utilizan para almacenar cualquier tipo de dato como strings,numeros o boolean.
 
-* Objetos:
+* Objetos: son contenedores para valores de datos,es decir se puede agrupar todo tipo de datos y puede contener varias variables y funciones.
 
-* Variables:
+* Variables:es un elemento que almacena y hace refencia a otro valor, que va a ser utilizado en el programa.
 
-* Métodos:
+* Métodos: hay métodos para arrays,numeros,strings,orden superior,esto sirve para modificar las propiedades de objetos,añadir o quitar elementos a una lista,funciones que pueden tomar otra funcion como argumentos o devolver.
 
-* DOM:
+* DOM: es un arbol de nodos, que nos da la estructura a la página, y que cada nodo representa una parte del documento(HTML O XML)
 
-* Atributo de un tag HTML:
+* Atributo de un tag HTML: son valores que pueden cambiar o configurar elementos.
 
-* Selector CSS:
+* Selector CSS:permite seleccionar que elementos en el DOM se aplica la regla CSS, es decir si se quiere agregar un estilo por ejemplo a body tengo que utilizar el selector para la etiqueta body. 
 
-* Un función constructora:
+* Un función constructora: es una función diseñada para crear un nuevo objeto,al crear el nuevo objeto ya podemos llamar la funcion.
 
